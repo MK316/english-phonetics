fd30e5d2f728646cd5d1e72683dab4050f84b004
MK <99416359+MK316@users.noreply.github.com>
2025-07-16 22:41:37 +0900

    Update 〰️_Textbook.py

--- pages/〰️_Textbook.py
+++ pages/〰️_Textbook.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.markdown("Ladefoged, P. & Johnson, K. (2014) _A Course in Phonetics_, Cengage.")
+st.markdown("Ladefoged, P. & Johnson, K. (2014) _A Course in Phonetics_ (7th edition), Cengage.")
