741ecb4cbda937be585b6aba1b67b488805613d5
MK <99416359+MK316@users.noreply.github.com>
2025-07-16 22:43:50 +0900

    Update 〰️_Textbook.py

--- pages/〰️_Textbook.py
+++ pages/〰️_Textbook.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.markdown("Ladefoged, P. & Johnson, K. (2014) _A Course in Phonetics_ (7th edition), Cengage.")
 
-st.markdown("[Google Book link](https://books.google.co.kr/books/about/A_Course_in_Phonetics.html?id=U4XaAgAAQBAJ&redir_esc=y")
+st.markdown("[Google Book link](https://books.google.co.kr/books/about/A_Course_in_Phonetics.html?id=U4XaAgAAQBAJ&redir_esc=y)")
