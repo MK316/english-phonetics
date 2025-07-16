502e7b85566e3498dae4f7801c9363a088b37e4b
MK <99416359+MK316@users.noreply.github.com>
2025-07-16 22:54:55 +0900

    Update 〰️_Textbook.py

--- pages/〰️_Textbook.py
+++ pages/〰️_Textbook.py
@@ -7,25 +7,25 @@ st.markdown("[Google Book link](https://books.google.co.kr/books/about/A_Course_
 st.markdown("""
 ### Part I: Introductory Concepts
 
-**Chapter 1:** Articulation and Acoustics:
+##### Chapter 1: Articulation and Acoustics
 
 This chapter introduces the fundamental concepts of speech production, sound waves, and the articulatory gestures involved in producing different sounds. It covers the oronasal process, manners of articulation (stops, fricatives, approximants), and the articulation of vowel sounds.
 
-**Chapter 2:** Phonology and Phonetic Transcription:
+##### Chapter 2: Phonology and Phonetic Transcription:
 
 This chapter focuses on the practical application of phonetic knowledge. It covers the transcription of consonants and vowels using the International Phonetic Alphabet (IPA), introduces consonant and vowel charts, and delves into the field of phonology. 
 
 ### Part II: English Phonetics
 
-**Chapter 3:** The Consonants of English:
+##### Chapter 3: The Consonants of English:
 
 This chapter provides a detailed look at the consonants of English, including stop consonants, fricatives, affricates, nasals, and approximants. It also discusses overlapping gestures, rules for English consonant allophones, and diacritics.
 
-**Chapter 4:** English Vowels:
+##### Chapter 4: English Vowels:
 
 This chapter focuses on English vowels, covering vowel quality, the auditory vowel space, American and British vowels, diphthongs, rhotic vowels, unstressed syllables, tense and lax vowels, and rules for English vowel allophones.
 
-**Chapter 5:** English Words and Sentences:
+##### Chapter 5: English Words and Sentences:
 
 This chapter explores the practical application of phonetics in connected speech, including stress, degrees of stress, sentence rhythm, intonation, and target tones. 
 
