11b10505db3b26dc43c8733e09d915da7406a78c
MK <99416359+MK316@users.noreply.github.com>
2025-08-29 22:05:52 +0900

    Update 10🐾_Padlet_for_Sharing.py

--- pages/10🐾_Padlet_for_Sharing.py
+++ pages/10🐾_Padlet_for_Sharing.py
@@ -2,5 +2,8 @@ import streamlit as st
 st.set_page_config(page_title="Class Padlet Wall", layout="wide")
 
 st.header("🐾 Q & As: on Padlet")
-st.write("This Padlet serves as a dynamic hub for Q & As regarding the course. Personal inquiries can be directred via E-mail at mirankim@gnu.ac.kr")
+st.markdown("""
+    This Padlet serves as a dynamic hub for Q & As regarding the course.   
+    Personal inquiries can be directred via E-mail at _mirankim@gnu.ac.kr_  
+    """)
 st.components.v1.iframe("https://padlet.com/mirankim316/engphonetics", width=700, height=600)
