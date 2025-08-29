37fd91eab3f6c8efd48b1075a5c7eef54f2e5336
MK <99416359+MK316@users.noreply.github.com>
2025-08-29 22:08:59 +0900

    Update 10🐾_Padlet_for_Sharing.py

--- pages/10🐾_Padlet_for_Sharing.py
+++ pages/10🐾_Padlet_for_Sharing.py
@@ -3,7 +3,6 @@ st.set_page_config(page_title="Class Padlet Wall", layout="wide")
 
 st.header("🐾 Sharing for classroom activities")
 st.markdown("""
-    This Padlet serves as a dynamic hub for Q & As regarding the course.   
-    Personal inquiries can be directred via E-mail at _mirankim@gnu.ac.kr_  
+    This Padlet serves as a dynamic hub for Q & As regarding the course. Personal inquiries can be directred via E-mail at _mirankim@gnu.ac.kr_  
     """)
 st.components.v1.iframe("https://padlet.com/mirankim316/engphonetics", width=800, height=600)
