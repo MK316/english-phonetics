2b934c8fb0a96c39db5fa5d747aaa0b344c0eb09
MK <99416359+MK316@users.noreply.github.com>
2025-08-29 22:06:30 +0900

    Update 10🐾_Padlet_for_Sharing.py

--- pages/10🐾_Padlet_for_Sharing.py
+++ pages/10🐾_Padlet_for_Sharing.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(page_title="Class Padlet Wall", layout="wide")
 
-st.header("🐾 Q & As: on Padlet")
+st.header("🐾 Sharing for classroom activities")
 st.markdown("""
     This Padlet serves as a dynamic hub for Q & As regarding the course.   
     Personal inquiries can be directred via E-mail at _mirankim@gnu.ac.kr_  
