9b7588968e7eaaf913b271f51e7b35a61bd27b2b
MK <99416359+MK316@users.noreply.github.com>
2025-08-31 23:16:12 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
+
 st.markdown("### English Phonetics")
+
+img = "https://github.com/MK316/english-phonetics/raw/main/pages/images/bg02.png"  # e.g., "images/figure.png" or a PIL Image
+
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    st.image(img, use_container_width=True)  # or: width=400
+
