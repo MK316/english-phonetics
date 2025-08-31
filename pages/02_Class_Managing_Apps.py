a7f67f0d257645f2c0809ad6c739f98d7391dcaa
MK <99416359+MK316@users.noreply.github.com>
2025-08-31 19:45:04 +0900

    Update 02_Class_Managing_Apps.py

--- pages/02_Class_Managing_Apps.py
+++ pages/02_Class_Managing_Apps.py
@@ -91,7 +91,7 @@ with tabs[2]:
         selected_course = st.selectbox("🌱 Step 2: Select Course for Grouping", course_list)
 
         # Step 2: Group size input
-        st.markdown("#### 🌱 Step3: Group Settings (Currently 17 students: 3*3G and 4*2G")
+        st.markdown("##### 🌱 Step3: Group Settings (Currently 17 students: 3*3G and 4*2G)")
         num_group3 = st.number_input("Number of 3-member groups", min_value=0, step=1)
         num_group4 = st.number_input("Number of 4-member groups", min_value=0, step=1)
 
