2a2f27232e87862ddbcc22076c92b5b159e2c0a4
MK <99416359+MK316@users.noreply.github.com>
2026-02-08 19:24:19 +0900

    Update 3🐾_Padlet_for_Sharing.py

--- pages/3🐾_Padlet_for_Sharing.py
+++ pages/3🐾_Padlet_for_Sharing.py
@@ -5,4 +5,4 @@ st.header("🐾 Sharing for classroom activities")
 st.markdown("""
     This Padlet serves as a dynamic hub for Q & As regarding the course. Personal inquiries can be directred via E-mail at _mirankim@gnu.ac.kr_  
     """)
-st.components.v1.iframe("https://padlet.com/mirankim316/engphonetics", width=800, height=600)
+st.components.v1.iframe("https://padlet.com/mirankim316/phonetics26", width=800, height=600)
