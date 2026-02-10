f4fca9cfd21adc62cc4ea98d737de0b6a8336d01
MK <99416359+MK316@users.noreply.github.com>
2026-02-10 12:17:56 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 
-img = "https://github.com/MK316/english-phonetics/raw/main/pages/images/bg02.png"  # e.g., "images/figure.png" or a PIL Image
+img = "https://github.com/MK316/english-phonetics/raw/main/pages/images/phonetics-bg-new.png"  # e.g., "images/figure.png" or a PIL Image
 
 col1, col2, col3 = st.columns([0.5, 3, 0.5])
 with col2:
