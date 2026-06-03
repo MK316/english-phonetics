042509f50d8bc89959dc2238378b06b5a79e7ed9
MK <99416359+MK316@users.noreply.github.com>
2026-06-04 00:00:00 +0900

    Update 50🔰_Chapter_5.py

--- pages/50🔰_Chapter_5.py
+++ pages/50🔰_Chapter_5.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.markdown("#### Chapter 5. English Words and Sentences")
+
+st.write("Group activities: TCE")
+st.markdown("""
+ 💧 G1: 2007  
+ 💧 G2: 2010  
+ 💧 G3: 2016  
+ 💧 G4: 2017  
+ 💧 G5: 2018  
+ 💧 G6: 2019
+  """)
